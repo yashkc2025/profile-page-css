17f185dad757343943c6c800e586e2e2f54600bb
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-08 01:51:39 +0530

    final comit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
         <div className="hero">
             <h3>About</h3>
             <p>Mount Fuji is the tallest mountain in Japan. standing at 3776 meters. 
-                Mount Fuji is the single most popular site in Japan, for both Japanese and
+                Mount Fuji is the most popular site in Japan, for both Japanese and
                 Foreign tourists.
             </p>
         </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,9 @@ export default function Intro() {
                 <h2>Laura Smith</h2>
                 {/* <h4>Frontend Developer</h4> */}
             </div>
-            <button>Email</button>
+            <button className="btn1"><a href="#" className="fa fa-linkedin"></a></button>
+            <span>    </span>
+            <button className="btn2"><a href="#" className="fa fa-envelope"></a></button>
         </div>
     );
 }
